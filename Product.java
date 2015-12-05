1a2b0550d2698ac4228ad74c54c781ca2a3384d5
Okonos <tepyzboj@gmail.com>
2015-12-05 23:40:02 +0100

    Więcej reguł

--- Product.java
+++ Product.java
@@ -7,12 +7,12 @@ public class Product {
 	private boolean gluten;
 	private boolean lactose;
 	
-	public Product(String name, boolean meat)//, boolean gluten, boolean lactose)
+	public Product(String name, boolean meat, boolean gluten, boolean lactose)
 	{
 		this.name = name;
 		this.meat = meat;
-		this.gluten = false;
-		this.lactose = false;
+		this.gluten = gluten;
+		this.lactose = lactose;
 	}
 	
 	public String Name()
